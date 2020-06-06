f57753ae03fb4c21102cc59d948d46fdba40c5b0
deyizhang <deyizhang2003@gmail.com>
2020-06-06 16:30:35 -0400

    Commit

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import praw
 
 reddit = praw.Reddit(client_id="qqV8kmfjpDEE9w",
                      client_secret="e6iVDV92IM7LyQRA1TOdPbYbT4w",
-                     user_agent="wsbcommentscraper")
+                     user_agent="wsbcommentscraper"
+                     )
+sub = reddit.submission("wallstreetbets")
+pinnedPost = sub.hot(limit =1)
+arr_comment = []
+counter = 500
+
+for post in pinnedPost:
+    post.comments.replace_more(limit=0)
+    for comment in post.comments.list():
+        arr_comment.append(comment)
 
 
-hot_posts = reddit.subreddit("wallstreetbets").hot(limit=10)
-for post in hot_posts:
-    print(post.title)
+
+
+print(arr_comment)
 
