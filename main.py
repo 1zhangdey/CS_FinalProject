67826bbe714809cdc10887fe26d2e6d9e78f7e93
deyizhang <deyizhang2003@gmail.com>
2020-06-15 22:40:47 -0400

    Commit

--- main.py
+++ main.py
@@ -1,42 +1,35 @@
 import praw
-import tkinter as tk
+import time
 
 
-window = tk.Tk()
-
-
-label = tk.Label(text= "r/WSB Comment Scraper!!!", foreground = "white",background = "black",)
-label.pack()
-
-button = tk.Button(text = "Start Program", width = 40, height = 30, )
-button.pack()
-
-
-window.mainloop()
-
-
-
-
-
-
+arr_comments = []
 
 reddit = praw.Reddit(client_id="qqV8kmfjpDEE9w",
                      client_secret="e6iVDV92IM7LyQRA1TOdPbYbT4w",
                      user_agent="wsbcommentscraper"
                      )
+discussionPost = reddit.subreddit("wallstreetbets").hot(limit=1)
 
-arr_comments = []
+def scrape_comments(state):
+    if state == True:
+        for post in discussionPost:
+            print("Running, Please Be Patient")
+            post.comments.replace_more(limit=1)
+            for comment in post.comments:
+                try:
+                    arr_comments.append(comment.body)
+                except AttributeError:
+                    pass
+    print("Successfully Scraped The Comments")
 
-discussionPost = reddit.subreddit('wallstreetbets').hot(limit=1)
 
-
-
-def scrape_comments():
-    for post in discussionPost:
-        post.comments.replace_more(limit=4)
-        for comment in post.comments:
-            try:
-                arr_comments.append(comment.body)
-            except AttributeError:
-                pass
-
+#start of program
+print("Welcome to the Reddit Comment Scraper!")
+time.sleep(3)
+print("Booting Up")
+time.sleep(1)
+subreddit = str(input("What Subreddit can I scrap for you today?"))
+time.sleep(2)
+print("Very nice choice sir")
+time.sleep(1)
+print("Accessing Subreddit")
